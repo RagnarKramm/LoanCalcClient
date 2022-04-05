d7784a51b385daa265ac21413b6e4ecc03c8fe66
Ragnar <rakram@ttu.ee>
2022-04-06 00:53:02 +0300

    Added dist folder

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'my-app.css'
-import Aurelia, { RouterConfiguration } from 'aurelia';
-import { MyApp } from './my-app';
-
-Aurelia
-  .register(RouterConfiguration)
-  .app(MyApp)
-  .start();
